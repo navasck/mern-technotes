edff4abf5f55e8e41f5030f402c8c9c07298c05c
ABDUL NAVAS CK <cknavas123@gmail.com>
2024-02-05 20:03:16 +0530

    user role based access control & permission done

--- client/src/features/auth/Welcome.jsx
+++ client/src/features/auth/Welcome.jsx
@@ -1,28 +1,42 @@
 import { Link } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth';
 
 const Welcome = () => {
+  const { username, isManager, isAdmin } = useAuth();
+  const date = new Date();
+  const today = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'full',
+    timeStyle: 'long',
+  }).format(date);
 
-    const date = new Date()
-    const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
+  const content = (
+    <section className='welcome'>
+      <p>{today}</p>
 
-    const content = (
-        <section className="welcome">
+      <h1>Welcome {username}!</h1>
 
-            <p>{today}</p>
+      <p>
+        <Link to='/dash/notes'>View techNotes</Link>
+      </p>
 
-            <h1>Welcome!</h1>
+      <p>
+        <Link to='/dash/notes/new'>Add New techNote</Link>
+      </p>
 
-            <p><Link to="/dash/notes">View techNotes</Link></p>
+      {(isManager || isAdmin) && (
+        <p>
+          <Link to='/dash/users'>View User Settings</Link>
+        </p>
+      )}
 
-            <p><Link to="/dash/notes/new">Add New techNote</Link></p>
+      {(isManager || isAdmin) && (
+        <p>
+          <Link to='/dash/users/new'>Add New User</Link>
+        </p>
+      )}
+    </section>
+  );
 
-            <p><Link to="/dash/users">View User Settings</Link></p>
-
-            <p><Link to="/dash/users/new">Add New User</Link></p>
-
-        </section>
-    )
-
-    return content
-}
+  return content;
+};
 export default Welcome
